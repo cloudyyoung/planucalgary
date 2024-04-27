ea75e485b877f0d233349bb00f4809f42e4acb5b
Yunfan Yang <cloudy.young@outlook.com>
2024-04-27 15:12:21 -0600

    Install flowbite react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const flowbite = require("flowbite-react/tailwind");
+
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+    flowbite.content(),
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+  ],
 }
 
