1ac05febf99dd4c47f0b63d411e6a5ad19339f5a
Yunfan Yang <cloudy.young@outlook.com>
2024-04-28 05:56:13 -0600

    Update Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -179,10 +179,10 @@ const Home = () => {
         </header>
 
         <main className='px-4 md:px-6 py-4 flex flex-row flex-nowrap flex-1 h-full gap-3 overflow-x-auto'>
-          <div className='bg-white rounded-3xl h-full w-[30%] min-w-[26rem] max-w-[30rem] flex-none'></div>
-          <div className='bg-white rounded-3xl h-full w-[30%] min-w-[26rem] max-w-[30rem] flex-none'></div>
-          <div className='bg-white rounded-3xl h-full w-[30%] min-w-[26rem] max-w-[30rem] flex-none'></div>
-          <div className='bg-white rounded-3xl h-full w-[30%] min-w-[26rem] max-w-[30rem] flex-none'></div>
+          <div className='bg-white rounded-3xl h-full w-[20rem] sm:w-[20rem] md:w-[22rem] lg:w-[22rem] xl:w-[32rem] flex-none'></div>
+          <div className='bg-white rounded-3xl h-full w-[20rem] sm:w-[20rem] md:w-[22rem] lg:w-[26rem] xl:w-[32rem] flex-none'></div>
+          <div className='bg-white rounded-3xl h-full w-[20rem] sm:w-[20rem] md:w-[22rem] lg:w-[26rem] xl:w-[32rem] flex-none'></div>
+          <div className='bg-white rounded-3xl h-full w-[20rem] sm:w-[20rem] md:w-[22rem] lg:w-[26rem] xl:w-[32rem] flex-none'></div>
         </main>
       </div>
     </>
