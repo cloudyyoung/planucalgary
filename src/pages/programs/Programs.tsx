9a667fea5c20fc724951fdddc200582e766ffe15
Yunfan Yang <cloudy.young@outlook.com>
2024-04-29 13:40:17 -0600

    Add add program button

--- src/pages/programs/Programs.tsx
+++ src/pages/programs/Programs.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@components"
-import { AcademicCapIcon, ArrowRightCircleIcon } from "@heroicons/react/24/solid"
+import { AcademicCapIcon, ArrowRightCircleIcon, PlusIcon } from "@heroicons/react/24/solid"
 
 const Programs = () => {
   return (
@@ -9,15 +9,23 @@ const Programs = () => {
       </header>
 
       <main className="px-app py-4 flex flex-row gap-4 flex-1">
-        <div className="flex flex-col w-72 gap-1">
-          <Program displayName="Bachelor of Science (BSc) in Actuarial Science - Honours" type="ACP" isSelected />
-          <Program displayName="Bachelor of Science (BSc) in Biomechanics" type="ACP" />
-          <Program displayName="Minor: Software Engineering" type="MIN" />
-          <Program displayName="Minor: Data Science" type="MIN" />
-          <Program displayName="Embedded Certificate: Creative Writing" type="EMC" />
+        <div className="flex flex-col justify-between">
+          <div className="flex flex-col w-72 gap-1">
+            <Program displayName="Bachelor of Science (BSc) in Actuarial Science - Honours" type="ACP" isSelected />
+            <Program displayName="Bachelor of Science (BSc) in Biomechanics" type="ACP" />
+            <Program displayName="Minor: Software Engineering" type="MIN" />
+            <Program displayName="Minor: Data Science" type="MIN" />
+            <Program displayName="Embedded Certificate: Creative Writing" type="EMC" />
+          </div>
+          <div>
+            <Button variant="filled">
+              <PlusIcon className="w-5 mr-2 flex-none" />
+              Add program
+            </Button>
+          </div>
         </div>
         <div className="bg-surface rounded-xl p-6 flex-1">
-          <div className="font-serif">Bachelor of Science (BSc) in Biomechanics</div>
+          <div className="font-serif text-5xl py-4 text-primary">Bachelor of Science (BSc) in Biomechanics</div>
         </div>
       </main>
     </>
@@ -35,7 +43,7 @@ const Program = ({
   type: type,
   isSelected: is_selected = false
 }: ProgramProps) => {
-  const iconClassNames = "w-5 mr-3 flex-none"
+  const iconClassNames = "w-5 mr-2 flex-none"
 
   return (
     <Button variant={is_selected ? "tonal" : "text"} className="justify-start text-left">
