29241a7f82f151bd1db4c620a396935f73a96390
Yunfan Yang <cloudy.young@outlook.com>
2024-04-29 13:55:28 -0600

    Twist layout

--- src/pages/programs/Programs.tsx
+++ src/pages/programs/Programs.tsx
@@ -4,18 +4,19 @@ import { AcademicCapIcon, ArrowRightCircleIcon, PlusIcon } from "@heroicons/reac
 const Programs = () => {
   return (
     <>
-      <header className="bg-inherit">
-        <h1 className="px-app py-2 title">Programs</h1>
-      </header>
-
-      <main className="px-app py-4 flex flex-row gap-4 flex-1">
+      <main className="px-app py-4 pt-0 flex flex-row gap-4 flex-1">
         <div className="flex flex-col justify-between">
-          <div className="flex flex-col w-72 gap-1">
-            <Program displayName="Bachelor of Science (BSc) in Actuarial Science - Honours" type="ACP" isSelected />
-            <Program displayName="Bachelor of Science (BSc) in Biomechanics" type="ACP" />
-            <Program displayName="Minor: Software Engineering" type="MIN" />
-            <Program displayName="Minor: Data Science" type="MIN" />
-            <Program displayName="Embedded Certificate: Creative Writing" type="EMC" />
+          <div className="flex flex-col gap-6">
+            <header className="bg-inherit">
+              <h1 className="title pt-2">Programs</h1>
+            </header>
+            <div className="flex flex-col w-72 gap-1">
+              <Program displayName="Bachelor of Science (BSc) in Actuarial Science - Honours" type="ACP" isSelected />
+              <Program displayName="Bachelor of Science (BSc) in Biomechanics" type="ACP" />
+              <Program displayName="Minor: Software Engineering" type="MIN" />
+              <Program displayName="Minor: Data Science" type="MIN" />
+              <Program displayName="Embedded Certificate: Creative Writing" type="EMC" />
+            </div>
           </div>
           <div>
             <Button variant="filled">
@@ -25,7 +26,12 @@ const Programs = () => {
           </div>
         </div>
         <div className="bg-surface rounded-xl p-6 flex-1">
-          <div className="font-serif text-5xl py-4 text-primary">Bachelor of Science (BSc) in Biomechanics</div>
+          <div className="mx-auto max-w-7xl">
+            <div className="font-serif text-5xl py-20 text-primary">Bachelor of Science (BSc) in Biomechanics</div>
+            <div className="leading-8">
+              <h3><strong>Contact Information </strong></h3><p>Website: <a href="https://arts.ucalgary.ca/anthropology-archaeology" rel="noopener noreferrer nofollow"><strong><u>arts.ucalgary.ca/anthropology-archaeology</u></strong></a>.</p><h3><strong>For Program Advice </strong></h3><p>Students should consult an undergraduate program advisor in the Arts Students’ Centre for information and advice on their overall program requirements. Advising contact information can be found online: <a href="https://arts.ucalgary.ca/current-students/undergraduate/academic-advising" rel="noopener noreferrer nofollow"><strong><u>arts.ucalgary.ca/advising</u></strong></a>.</p><h3><strong>Introduction</strong></h3><p>The Department of Anthropology and Archaeology offers instruction in African Studies, Archaeology, Biological Anthropology, Social and Cultural Anthropology, and Global Development Studies.</p><p>The Department of Anthropology and Archaeology takes a comparative, cross-cultural, and cross-species perspective to understand human beings. Anthropologists and archaeologists consider how humans evolved, how they shape—and are shaped by—their culture, and seek to understand the records they have left behind. Students are encouraged to take one of the field schools to gain hands-on experience.</p><p>Students wishing to emphasize the social sciences and humanities in their Anthropology or Archaeology program should register for the BA degree. Those wishing to emphasize the natural and biological sciences should register for the BSc degree. It is recommended that first-year students in any of these programs register in&nbsp;Anthropology 201,&nbsp;203, and&nbsp;Archaeology 201.</p><p>The BA in Anthropology focuses on social and cultural anthropology and adopts a cross-cultural perspective. Courses seek to foster an understanding and appreciation of the wide variety of cultures in the world and provide critical insights into how people actually live and how they negotiate the challenges created by globalization.</p>
+            </div>
+          </div>
         </div>
       </main>
     </>
