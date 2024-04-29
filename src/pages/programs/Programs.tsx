b1c4df6c2418441491f1e33d4122285ec79d58c7
Yunfan Yang <cloudy.young@outlook.com>
2024-04-29 12:54:46 -0600

    Construct programs

--- src/pages/programs/Programs.tsx
+++ src/pages/programs/Programs.tsx
@@ -1,11 +1,34 @@
+import { Button } from "@components"
 
 const Programs = () => {
   return (
     <>
       <header className="bg-surface">
-        <h1 className="px-4 md:px-6 py-2 title">Programs</h1>
+        <h1 className="px-app py-2 title">Programs</h1>
       </header>
+
+      <main className="px-app py-4 flex flex-row gap-4 flex-1">
+        <div className="flex flex-col w-72 gap-2">
+          <Program name="Bachelor of Science (BSc) in Actuarial Science - Honours" />
+          <Program name="Bachelor of Science (BSc) in Biomechanics" />
+        </div>
+        <div className="bg-white rounded-xl p-6 flex-1">
+          <div className="font-serif">Bachelor of Science (BSc) in Biomechanics</div>
+        </div>
+      </main>
     </>
+  )
+}
+
+interface ProgramProps {
+  name: string
+}
+
+const Program = ({ name }: ProgramProps) => {
+  return (
+    <Button variant="outline" className="text-left">
+      {name}
+    </Button>
   )
 }
 
--- src/pages/terms/Terms.tsx
+++ src/pages/terms/Terms.tsx
@@ -3,10 +3,10 @@ const Terms = () => {
   return (
     <>
       <header className="bg-surface">
-        <h1 className="px-4 md:px-6 py-2 title">Terms</h1>
+        <h1 className="px-app py-2 title">Terms</h1>
       </header>
 
-      <main className='px-4 md:px-6 py-4 flex flex-row flex-nowrap flex-1 h-full gap-3 overflow-x-auto'>
+      <main className='px-app py-4 flex flex-row flex-nowrap flex-1 h-full gap-3 overflow-x-auto'>
         <div className='bg-white rounded-3xl h-full w-[20rem] sm:w-[20rem] md:w-[22rem] lg:w-[22rem] xl:w-[32rem] flex-none'></div>
         <div className='bg-white rounded-3xl h-full w-[20rem] sm:w-[20rem] md:w-[22rem] lg:w-[26rem] xl:w-[32rem] flex-none'></div>
         <div className='bg-white rounded-3xl h-full w-[20rem] sm:w-[20rem] md:w-[22rem] lg:w-[26rem] xl:w-[32rem] flex-none'></div>
