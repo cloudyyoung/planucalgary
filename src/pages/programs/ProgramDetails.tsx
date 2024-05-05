5679e2912dd764485a83c254327c67f6135b23c7
Yunfan Yang <cloudy.young@outlook.com>
2024-05-04 23:24:59 -0600

    Update ProgramDetails.tsx

--- src/pages/programs/ProgramDetails.tsx
+++ src/pages/programs/ProgramDetails.tsx
@@ -8,39 +8,39 @@ interface ProgramDetailsProps {
 const ProgramDetails = ({
   programData
 }: ProgramDetailsProps) => {
+  const programRequirements = programData.requisites.find((requisite) => requisite.name === 'Program Requirements')
+  console.log(programRequirements.rules)
+
   return (
     <>
-      {
-        programData.requisites.map((requisite, index) => (
-          <div key={index} className="py-4">
-            <div className="font-bold font-serif mb-2">{requisite.name}</div>
-            <Accordion>
-              {
-                requisite.rules.map((rule: any, index: number) => (
-                  <Accordion.Panel key={index}>
-                    <Accordion.Title>{rule.name}</Accordion.Title>
-                    <Accordion.Content>
-                      <div dangerouslySetInnerHTML={{ __html: rule.description }} />
-                      {rule.condition}
-                      {rule.value.values.map((value: any, index: number) => (
-                        <div key={index}>
-                          {/* <div key={index}>{value.logic}</div> */}
-                          {
-                            value.values.map((value: any, index: number) => (
-                            <div key={index}>{value.name}</div>
-                          ))
-                          }
-                        </div>
-                      ))}
-                      <div dangerouslySetInnerHTML={{ __html: rule.notes }} />
-                    </Accordion.Content>
-                  </Accordion.Panel>
-                ))
-              }
-            </Accordion>
-          </div>
-        ))
-      }
+      <div className="py-4">
+        {
+          programRequirements.rules.map((rule: any, index: number) => (
+            <>
+              <div className="text-lg font-semibold">{rule.name}</div>
+              {rule.credits && <div className="text-sm text-on-surface-low">Earn at least {rule.credits} credits: </div>}
+              <Accordion>
+                {
+                  rule.value.values.map((value: any, index_outer: number) => value.values.map((rule: any, index: number) => (
+                    <Accordion.Panel key={`${index_outer}-${index}`}>
+                      <Accordion.Title>{rule.name}</Accordion.Title>
+                      <Accordion.Content>
+                        {rule.structure && rule.structure.condition}
+                        {rule.structure && rule.structure.rules.map((srule: any, index: number) => (
+                          <div key={index}>
+                            <div>{srule.value.subject_code} {srule.value.course_number}</div>
+                            <div>{srule.value.long_name}</div>
+                          </div>
+                        ))}
+                      </Accordion.Content>
+                    </Accordion.Panel>
+                  )))
+                }
+              </Accordion>
+            </>
+          ))
+        }
+      </div>
     </>
   )
 }
