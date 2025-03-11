0ae52e0190550e8b85447442125c28938417da94
Yunfan Yang <cloudy.young@outlook.com>
2025-03-11 03:04:03 -0600

    Make text input component

--- src/pages/courses/Courses.tsx
+++ src/pages/courses/Courses.tsx
@@ -1,6 +1,31 @@
+import { useState } from "react"
+import { TextInput } from "flowbite-react"
+
+import { useCourses } from "src/hooks/useCourses"
 
 export const Courses = () => {
+  const [keywords, setKeywords] = useState('')
+
+  const { courses, total } = useCourses({
+    keywords,
+  })
+
   return (
-    <div>Courses</div>
+    <>
+      <header>
+        <h1 className="px-app py-2 title">Courses</h1>
+      </header>
+
+      <main className='px-app py-4'>
+        <TextInput sizing="lg" placeholder="Search courses" value={keywords} onChange={(e) => setKeywords(e.target.value)} />
+
+        {courses.map((course) => (
+          <div key={course.id} className="border-b border-gray-200 py-2">
+            <div>{course.subject_code} {course.course_number}</div>
+            <h2>{course.long_name}</h2>
+          </div>
+        ))}
+      </main>
+    </>
   )
 }
--- src/theme.ts
+++ src/theme.ts
@@ -28,6 +28,30 @@ const theme: CustomFlowbiteTheme = {
     },
   },
 
+  textInput: {
+    "field": {
+      "base": "block w-full disabled:cursor-not-allowed disabled:opacity-50",
+      "input": {
+        "sizes": {
+        "sm": "p-4 sm:text-sm",
+        "md": "p-4.5 text-base",
+        "lg": "p-5 sm:text-xl"
+      },
+      "colors": {
+        "gray": "border-none bg-container text-gray-900 focus:ring-2 focus:ring-primary-50 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:ring-primary-500",
+        "info": "border-none bg-primary-50 text-primary-90 placeholder-primary-70 focus:ring-2 focus:ring-primary-500 dark:bg-primary-100  dark:focus:ring-primary-500",
+        "failure": "border-red-500 bg-red-50 text-red-900 placeholder-red-700 focus:ring-2 focus:ring-red-500 dark:bg-red-100 dark:focus:ring-red-500",
+        "warning": "border-yellow-500 bg-yellow-50 text-yellow-900 placeholder-yellow-700 focus:ring-2 focus:ring-yellow-500 dark:bg-yellow-100 dark:focus:ring-yellow-500",
+        "success": "border-green-500 bg-green-50 text-green-900 placeholder-green-700 focus:ring-2 focus:ring-green-500 dark:bg-green-100 dark:focus:ring-green-500"
+      },
+        "withAddon": {
+          "on": "rounded-full",
+          "off": "rounded-full"
+        },
+      }
+    }
+  },
+
   accordion: {
     "root": {
       "base": "border-outline-variant",
