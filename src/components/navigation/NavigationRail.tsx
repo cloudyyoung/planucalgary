00e7b070399d3808b693039f7e71d1d8230ecd64
Yunfan Yang <cloudy.young@outlook.com>
2025-04-13 16:58:15 -0600

    Add ripple

--- src/components/navigation/NavigationRail.tsx
+++ src/components/navigation/NavigationRail.tsx
@@ -17,6 +17,10 @@ const NavigationRail = ({ alignment = "top" }: NavigationRailProps) => {
   const routes = [
     { label: "Courses", icon: "book", path: "/courses" },
     { label: "Terms", icon: "calendar_today", path: "/terms" },
+    { label: "Programs", icon: "school", path: "/programs" },
+    { label: "Admin Requisites", icon: "manage_accounts", path: "/admin/requisites" },
+    { label: "Admin Courses", icon: "manage_accounts", path: "/admin/courses" },
+    { label: "Prototype Buttons", icon: "manage_accounts", path: "/prototypes/buttons" },
   ]
 
   return (
@@ -65,17 +69,19 @@ const NavigationRailButton = ({ icon, label, active = false, className, ...args 
         baseCommon,
         stateLayerCommon,
         "material-symbols-outlined [font-variation-settings:'FILL'_0,'wght'_400,'opsz'_24] h-8 w-14 mx-3 duration-100",
+        "before:absolute before:pointer-events-none before:top-0 before:left-0 before:w-full before:h-full before:rounded-2xl before:transition-all before:-z-20 before:duration-200 before:bg-secondary-container",
         active
           ? "[font-variation-settings:'FILL'_1,'wght'_400,'opsz'_24] group-hover:[font-variation-settings:'FILL'_1,'wght'_600,'opsz'_24] group-active:[font-variation-settings:'FILL'_1,'wght'_300,'opsz'_24]"
           : "[font-variation-settings:'FILL'_0,'wght'_400,'opsz'_24] group-hover:[font-variation-settings:'FILL'_0,'wght'_600,'opsz'_24] group-active:[font-variation-settings:'FILL'_0,'wght'_300,'opsz'_24]",
-        active ? "bg-secondary-container text-on-secondary-container" : "text-on-surface-variant",
+        active ? "text-on-secondary-container" : "text-on-surface-variant",
+        active ? "before:scale-x-100 before:opacity-100" : "before:scale-x-[0.32] before:opacity-0",
         active ? "group-hover:text-on-secondary-container group-hover:after:bg-on-surface/8" : "group-hover:text-on-surface group-hover:after:bg-on-surface/8",
         active ? "group-active:text-on-secondary-container group-active:after:bg-on-surface/12" : "group-active:text-on-surface group-active:after:bg-on-surface/12",
       )}>
         {icon}
       </span>
       <span className={clsx(
-        "text-xs duration-100",
+        "text-xs duration-100 w-14 leading-4",
         "group-hover:[font-variation-settings:'wght'_700] group-active:[font-variation-settings:'wght'_300]",
         active ? "font-bold text-on-surface" : "font-500 text-on-surface-variant",
         active ? "group-hover:text-on-surface" : "group-hover:text-on-surface",
