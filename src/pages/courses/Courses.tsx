809ff9f326a7b3860f40fcc67008d8cb785b9a85
Yunfan Yang <cloudy.young@outlook.com>
2025-04-13 06:02:09 -0600

    Update Courses.tsx

--- src/pages/courses/Courses.tsx
+++ src/pages/courses/Courses.tsx
@@ -14,10 +14,6 @@ export const Courses = () => {
 
   return (
     <>
-      <header>
-        <h1 className="px-app title">Courses</h1>
-      </header>
-
       <main className='px-app py-4 flex flex-col flex-nowrap flex-1 h-full gap-3 overflow-x-auto'>
         <TextInput sizing="lg" placeholder="Search courses by keywords" value={keywords} onChange={(e) => setKeywords(e.target.value)} />
 
@@ -28,7 +24,7 @@ export const Courses = () => {
           <FilterChip>Archived</FilterChip>
         </div> */}
 
-        <div className="overflow-y-auto rounded-3xl bg-surface">
+        <div className="">
           <List unstyled>
             {courses.map((course: any) => (
               <List.Item key={course.id} className="text-on-surface p-4 cursor-pointer relative after:content-[''] after:pointer-events-none after:absolute after:top-0 after:bottom-0 after:left-0 after:right-0 after:bg-none after:transition-colors hover:after:bg-on-surface/8">
