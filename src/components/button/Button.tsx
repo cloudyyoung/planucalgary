03a58c8c4a2033aed366fd49a34872137a95e046
Yunfan Yang <cloudy.young@outlook.com>
2025-04-13 03:41:50 -0600

    Refactor Button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,15 +1,46 @@
-import { Button as FlowbiteButton, ButtonProps as FlowbiteButtonProps } from "flowbite-react";
+import { ButtonHTMLAttributes, HTMLAttributes } from "react";
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge"
 
-interface ButtonProps extends Omit<FlowbiteButtonProps, "color" | "pill" | "outline"> {
-  variant?: "filled" | "tonal" | "outline" | "text"
-  priority?: "primary" | "secondary" | "tertiary"
+type ButtonAppearance = "filled" | "outlined" | "text" | "elevated" | "tonal"
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  appearance: ButtonAppearance
 }
 
-const Button = ({ variant = "text", priority = "primary", ...args }: ButtonProps) => {
-  const color = variant + "-" + priority
+const Button = ({ appearance = "text", className, ...args }: ButtonProps) => {
+  const lookup: Record<ButtonAppearance, HTMLAttributes<HTMLElement>["className"]> = {
+    filled: "bg-primary text-on-primary hover:shadow-md active:shadow-none disabled:bg-on-surface/12",
+    outlined: "text-primary border border-outline disabled:border-on-surface/12",
+    text: "text-primary px-3 hover:after:bg-primary/8 focus:after:bg-primary/12",
+    elevated: clsx(
+      "bg-surface-container-low text-primary shadow-md",
+      "hover:after:bg-primary/8 focus:after:bg-primary/12 active:shadow",
+      "disabled:bg-on-surface/12 disabled:text-on-surface/38 disabled:after:bg-transparent disabled:shadow-none",
+    ),
+    tonal: clsx(
+      "bg-secondary-container text-on-secondary-container hover:shadow active:shadow-none",
+      "after:bg-transparent hover:after:bg-on-secondary-container/8 focus:after:bg-on-secondary-container/12",
+      "disabled:bg-on-surface/12 disabled:text-on-surface/38 disabled:after:bg-transparent",
+    ),
+  }
+
+  const classNames: HTMLAttributes<HTMLElement>["className"] = twMerge(clsx(
+    "rounded-full after:rounded-full group relative flex items-center justify-center text-sm px-4 h-10 text-center font-medium transition-[color,background-color,border-color,text-decoration-color,fill,stroke,box-shadow] outline-none z-0",
+    "focus-visible:z-10 focus-visible:outline focus-visible:outline-2 focus-visible:outline-secondary focus-visible:outline-offset-2",
+    "disabled:pointer-events-none disabled:text-on-surface/38",
+
+    // State layer
+    "after:pointer-events-none after:absolute after:top-0 after:left-0 after:w-full after:h-full after:z-[-1] after:transition-[color,background-color,border-color,text-decoration-color,fill,stroke,box-shadow]",
+    "after:bg-on-primary/0 hover:after:bg-on-primary/8 focus:after:bg-on-primary/12",
+
+    lookup[appearance],
+
+    className,
+  ))
 
   return (
-    <FlowbiteButton color={color} pill={true} {...args} />
+    <button className={classNames} {...args} />
   )
 }
 
