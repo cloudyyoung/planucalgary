7e128072de370075d253a5c853d1fb71cdf1ca17
Yunfan Yang <cloudy.young@outlook.com>
2025-07-10 10:55:00 -0600

    Update Courses.tsx

--- src/pages/courses/Courses.tsx
+++ src/pages/courses/Courses.tsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { PlusIcon } from "@heroicons/react/24/solid"
 
 import { useCourses } from "@/hooks/useCourses"
-import { Button } from "@/components/ui/button"
+import { Button, ButtonProps } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Badge } from "@/components/ui/badge"
 import {
@@ -21,16 +21,21 @@ export const Courses = () => {
   return (
     <>
       <main className='px-4 py-4 space-y-2'>
-        <div className="sticky top-0 pt-2 z-20 bg-white rounded-b-md">
+        <div className="sticky top-0 py-2 z-20 bg-white">
           <Input className="w-full h-11" placeholder="Search courses by keywords" value={keywords} onChange={(e) => setKeywords(e.target.value)} />
-        </div>
 
-        {/*
-        <div className="flex flex-row gap-1">
-          <FilterChip>Undergraduate</FilterChip>
-          <FilterChip>Graduate</FilterChip>
-          <FilterChip>Archived</FilterChip>
-        </div> */}
+          <div className="flex flex-row gap-1 mt-2">
+            <FilterChip>Undergraduate</FilterChip>
+            <FilterChip>Graduate</FilterChip>
+            <FilterChip>100+</FilterChip>
+            <FilterChip>200+</FilterChip>
+            <FilterChip>300+</FilterChip>
+            <FilterChip>400+</FilterChip>
+            <FilterChip>500+</FilterChip>
+            <FilterChip>600+</FilterChip>
+            <FilterChip>700+</FilterChip>
+          </div>
+        </div>
 
         <div className="">
           <div className="flex flex-col w-full divide-y">
@@ -41,7 +46,7 @@ export const Courses = () => {
                     <Button variant="secondary" className="h-6 flex flex-row mr-4">
                       <PlusIcon className="w-5 h-5" />
                     </Button>
-                    <Badge variant="outline" className="font-bold font-mono rounded-md">
+                    <Badge variant="outline" className="font-bold font-mono rounded-md gap-1">
                       <span>{course.subject_code}</span>
                       <span>{course.course_number}</span>
                     </Badge>
@@ -50,7 +55,7 @@ export const Courses = () => {
                 </HoverCardTrigger>
                 <HoverCardContent className="l-0 w-[80vw] md:w-[30rem]" align="start" alignOffset={80}>
                   <div className="space-y-1">
-                    <Badge variant="default" className="font-bold font-mono rounded-md">
+                    <Badge variant="default" className="font-bold font-mono rounded-md gap-1">
                       <span>{course.subject_code}</span>
                       <span>{course.course_number}</span>
                     </Badge>
@@ -66,5 +71,11 @@ export const Courses = () => {
         </div>
       </main>
     </>
+  )
+}
+
+const FilterChip = ({ children, ...props }: ButtonProps) => {
+  return (
+    <Button className="rounded-full text-xs h-6" variant="secondary" {...props}>{children}</Button>
   )
 }
