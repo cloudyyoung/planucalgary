6a03f8f5c06de570238c26e817c9e719e4c62bb7
Yunfan Yang <cloudy.young@outlook.com>
2025-11-12 00:36:18 -0700

    Update utils.ts

--- server/src/zod/utils.ts
+++ server/src/zod/utils.ts
@@ -2,5 +2,5 @@ import { z } from "zod"
 
 export const LiteralSchema = z.union([z.string(), z.number(), z.boolean(), z.null()])
 export type Literal = z.infer<typeof LiteralSchema>
-export const JsonSchema: z.ZodType = z.lazy(() => z.union([LiteralSchema, z.array(JsonSchema), z.record(JsonSchema)]))
+export const JsonSchema: z.ZodType = z.lazy(() => z.union([LiteralSchema, z.array(JsonSchema), z.json()]))
 export type Json = z.infer<typeof JsonSchema>
