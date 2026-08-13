f0253124fb6aa5a8a785c7626278a1d6b59c0598
Yunfan Yang <cloudy.young@outlook.com>
2026-08-13 11:46:47 -0600

    Update trpc.ts

--- client/src/trpc.ts
+++ client/src/trpc.ts
@@ -4,7 +4,7 @@ import type { Router } from "../../server/src/trpc/router"
 
 const API_BASE_URL = "http://localhost:5150"
 const API_AUTHORIZATION =
-  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImU4OGI1OTVhLTU1NmMtNDQ0YS1iYjExLTk4ZjY1OTg2YWJjMSIsImVtYWlsIjoiY2xvdWR5LnlvdW5nQG91dGxvb2suY29tIiwiaWF0IjoxNzc0NDg4MzMwLCJleHAiOjE3Nzk2NzIzMzAsImlzcyI6InBsYW4tdWNhbGdhcnktYXBpIn0.V7FCrCrdboVk-xRh-VsSieL5Vk-Lm78SDkBOF-oKnWE"
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImU4OGI1OTVhLTU1NmMtNDQ0YS1iYjExLTk4ZjY1OTg2YWJjMSIsImVtYWlsIjoiY2xvdWR5LnlvdW5nQG91dGxvb2suY29tIiwiaWF0IjoxNzg2NjQzMTM2LCJleHAiOjE3ODc1MDcxMzYsImlzcyI6InBsYW4tdWNhbGdhcnktYXBpIn0.dhyxXT18Gu4ZnOAT6xLjGPjBaap3jBlByX6oOreajSw"
 
 export const queryClient = new QueryClient()
 
